bccee41f23d42dcfae2212b642ca6ee9f354ce11
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-05-19 20:42:21 +0530

    Update blockscoping.js

--- blockscoping.js
+++ blockscoping.js
@@ -42,3 +42,56 @@ function greet () {
   }
 }
 greet ();//hello
+import React, { useState } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const FilterableDataGrid = () => {
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+  const [filterText, setFilterText] = useState('');
+
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+  };
+
+  const handleFilterTextChange = (event) => {
+    setFilterText(event.target.value);
+  };
+
+  const applyFilter = () => {
+    gridApi.setQuickFilter(filterText);
+  };
+
+  return (
+    <div style={{ width: '100%', height: '500px' }}>
+      <input
+        type="text"
+        value={filterText}
+        onChange={handleFilterTextChange}
+        placeholder="Filter text..."
+      />
+      <button onClick={applyFilter}>Apply Filter</button>
+
+      <div className="ag-theme-alpine" style={{ height: '100%', width: '100%' }}>
+        <AgGridReact
+          onGridReady={onGridReady}
+          rowData={[
+            { id: 1, name: 'John Doe', age: 25 },
+            { id: 2, name: 'Jane Smith', age: 30 },
+            // Add more rows as needed
+          ]}
+        >
+          <AgGridColumn field="id" filter={true}></AgGridColumn>
+          <AgGridColumn field="name" filter={true}></AgGridColumn>
+          <AgGridColumn field="age" filter={true}></AgGridColumn>
+          {/* Add more columns as needed */}
+        </AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+export default FilterableDataGrid;
